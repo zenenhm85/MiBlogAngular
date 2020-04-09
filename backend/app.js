675449f32b2748852b2e7320b008fd1a25c6a94f
Zenén Hernández Martínez <zenenhernandez85@gmail.com>
2020-04-09 23:22:48 +0100

    comenzando con node

--- backend/app.js
+++ backend/app.js
@@ -1,2 +1,20 @@
 'use strict'
 
+//Crear modulos de node para crear servidor
+var express = require('express');
+var bodyParser =  require('body-parser');
+
+// Ejecutar express (http)
+var app = express();
+
+// Cargar ficheros rutas
+
+// Middlewares
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//CORS
+
+//Añadir prefijos a rutas
+
+//Exportar modulo (fichero actual)
